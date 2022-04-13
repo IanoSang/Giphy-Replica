96300ff4a379eb57d4b30e7b39d51de65d7bd888
IanoSang <ianosang18@gmail.com>
2022-04-13 13:16:41 +0300

    feat: Navbar Component- adds a navigation bar to for navigation to other components

---
+++ src/app/giphy.ts
@@ -0,0 +1,4 @@
+export class Giphy {
+  constructor(public id:any, public embed_url:any, public import_datetime:Date) {
+  }
+}
--- src/app/giphy/giphy.component.ts
+++ src/app/giphy/giphy.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GiphyComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
